9bbc4b471b65ab34fe7b25fe5cacdca56afee545
adrlm <adriellima.barros@gmail.com>
2025-03-30 19:54:49 -0300

    lombok annotations added

--- src/main/java/com/example/financeWallet/dto/BuyDTO.java
+++ src/main/java/com/example/financeWallet/dto/BuyDTO.java
@@ -1,7 +1,16 @@
 package com.example.financeWallet.dto;
 
 import com.example.financeWallet.entity.BuyEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.BeanUtils;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 
 public class BuyDTO {
     private Long id;
@@ -18,92 +27,4 @@ public class BuyDTO {
         BeanUtils.copyProperties(entity, this);
     }
 
-    public BuyDTO(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPurchasedCryptoCode() {
-        return purchasedCryptoCode;
-    }
-
-    public void setPurchasedCryptoCode(String purchasedCryptoCode) {
-        this.purchasedCryptoCode = purchasedCryptoCode;
-    }
-
-    public String getCryptoUsed() {
-        return cryptoUsed;
-    }
-
-    public void setCryptoUsed(String cryptoUsed) {
-        this.cryptoUsed = cryptoUsed;
-    }
-
-    public String getBuyDate() {
-        return buyDate;
-    }
-
-    public void setBuyDate(String buyDate) {
-        this.buyDate = buyDate;
-    }
-
-    public String getAmountCryptoPurchased() {
-        return amountCryptoPurchased;
-    }
-
-    public void setAmountCryptoPurchased(String amountCryptoPurchased) {
-        this.amountCryptoPurchased = amountCryptoPurchased;
-    }
-
-    public String getAmountSpent() {
-        return amountSpent;
-    }
-
-    public void setAmountSpent(String amountSpent) {
-        this.amountSpent = amountSpent;
-    }
-
-    public String getTaxCryptoCode() {
-        return taxCryptoCode;
-    }
-
-    public void setTaxCryptoCode(String taxCryptoCode) {
-        this.taxCryptoCode = taxCryptoCode;
-    }
-
-    public String getTaxAmount() {
-        return taxAmount;
-    }
-
-    public void setTaxAmount(String taxAmount) {
-        this.taxAmount = taxAmount;
-    }
-
-    public String getExchange() {
-        return exchange;
-    }
-
-    public void setExchange(String exchange) {
-        this.exchange = exchange;
-    }
-
-    @Override
-    public String toString() {
-        return "BuyDTO{" +
-                "id=" + id +
-                ", purchasedCryptoCode='" + purchasedCryptoCode + '\'' +
-                ", cryptoUsed='" + cryptoUsed + '\'' +
-                ", buyDate='" + buyDate + '\'' +
-                ", amountCryptoPurchased='" + amountCryptoPurchased + '\'' +
-                ", amountSpent='" + amountSpent + '\'' +
-                ", taxCryptoCode='" + taxCryptoCode + '\'' +
-                ", taxAmount='" + taxAmount + '\'' +
-                ", exchange='" + exchange + '\'' +
-                '}';
-    }
 }
