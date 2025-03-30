444911bc80da5e14ac1d9ab609e0a9d5b1b42fea
adrlm <adriellima.barros@gmail.com>
2025-03-30 19:54:04 -0300

    lombok annotations and p&l calculation added

--- src/main/java/com/example/financeWallet/dto/CurrencyDTO.java
+++ src/main/java/com/example/financeWallet/dto/CurrencyDTO.java
@@ -2,61 +2,44 @@ package com.example.financeWallet.dto;
 
 import com.example.financeWallet.entity.CurrencyEntity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.BeanUtils;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrencyDTO {
     private Long id;
     private String code; // code of the crypto bought
     private String codein; // name of the crypto used to buy
     private String bid; // current price of crypto bought
+    private BigDecimal profit;
 
     public CurrencyDTO(CurrencyEntity entity) {
         BeanUtils.copyProperties(entity, this);
     }
 
-    public CurrencyDTO() {
+    public BigDecimal getProfit() {
+        BigDecimal currentValue = convertToBigDecimal(bid);
+        BigDecimal buyPrice = BigDecimal.valueOf(5000);
+
+        return currentValue.subtract(buyPrice).setScale(2, RoundingMode.DOWN);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getCodein() {
-        return codein;
-    }
-
-    public void setCodein(String codein) {
-        this.codein = codein;
-    }
-
-    public String getBid() {
-        return bid;
-    }
-
-    public void setBid(String bid) {
-        this.bid = bid;
-    }
-
-    @Override
-    public String toString() {
-        return "CurrencyDTO{" +
-                "id=" + id +
-                ", code='" + code + '\'' +
-                ", codein='" + codein + '\'' +
-                ", bid='" + bid + '\'' +
-                '}';
+    private BigDecimal convertToBigDecimal(String value){
+        try{
+            return new BigDecimal(value);
+        }catch (NumberFormatException | NullPointerException e){
+            return BigDecimal.ZERO;
+        }
     }
 }
