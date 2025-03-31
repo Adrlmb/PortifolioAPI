cba6655e5820e054f46934aa73eb4e8fcbe4f53a
adrlm <adriellima.barros@gmail.com>
2025-03-31 01:58:10 -0300

    bigdecimal converter deleted

--- src/main/java/com/example/financeWallet/dto/CurrencyDTO.java
+++ src/main/java/com/example/financeWallet/dto/CurrencyDTO.java
@@ -20,24 +20,8 @@ public class CurrencyDTO {
     private String code; // code of the crypto bought
     private String codein; // name of the crypto used to buy
     private String bid; // current price of crypto bought
-    private BigDecimal profit;
 
     public CurrencyDTO(CurrencyEntity entity) {
         BeanUtils.copyProperties(entity, this);
-    }
-
-    public BigDecimal getProfit() {
-        BigDecimal currentValue = convertToBigDecimal(bid);
-        BigDecimal buyPrice = BigDecimal.valueOf(5000);
-
-        return currentValue.subtract(buyPrice).setScale(2, RoundingMode.DOWN);
-    }
-
-    private BigDecimal convertToBigDecimal(String value){
-        try{
-            return new BigDecimal(value);
-        }catch (NumberFormatException | NullPointerException e){
-            return BigDecimal.ZERO;
-        }
     }
 }
