2075c70ba1ec725769d87bb18a56767e5ca7671c
adrlm <adriellima.barros@gmail.com>
2025-03-31 02:01:04 -0300

    code refactoring: final variables and autowired constructor added

--- src/main/java/com/example/financeWallet/service/MasterService.java
+++ src/main/java/com/example/financeWallet/service/MasterService.java
@@ -18,48 +18,51 @@ import java.net.http.HttpResponse;
 
 @Service
 public class MasterService {
-    @Autowired
-    private CurrencyRepository currencyRepository;
-    @Autowired
-    private BuyRepository buyRepository;
 
-    public void insert(BuyDTO dto) throws IOException, InterruptedException {
-        CurrencyDTO currencyDTO = new CurrencyDTO();
-        BuyEntity buyEntity = new BuyEntity(dto);// pega o que foi digitado no post
-        currencyDTO = cryptoAPi(dto.getPurchasedCryptoCode(), dto.getCryptoUsed()); // grava os dados da api diretamento no dto de acordo com a crypto digitada anteriormente
-        CurrencyEntity finished = new CurrencyEntity(currencyDTO); // manda os dados para o banco de dados
-        currencyRepository.save(finished);//Salva na tabela currency
+    private final CurrencyRepository currencyRepository;
+    private final BuyRepository buyRepository;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public MasterService(CurrencyRepository currencyRepository, BuyRepository buyRepository){
+        this.currencyRepository = currencyRepository;
+        this.buyRepository = buyRepository;
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public void insert(BuyDTO buyDTO) throws IOException, InterruptedException {
+
+        CurrencyDTO currencyDTO = cryptoAPi(buyDTO.getPurchasedCryptoCode(), buyDTO.getCryptoUsed()); // grava os dados da api diretamento no dto de acordo com a crypto digitada anteriormente
+        CurrencyEntity currencyEntity = new CurrencyEntity(currencyDTO); // manda os dados para o banco de dados
+        BuyEntity buyEntity = new BuyEntity(buyDTO);// pega o que foi digitado no post
+        currencyRepository.save(currencyEntity);//Salva na tabela currency
         buyRepository.save(buyEntity);// Salva na tabela buy
     }
 
     public CurrencyDTO cryptoAPi (String code, String codein) throws IOException, InterruptedException {
-        CurrencyDTO dto;
         code = code.toUpperCase();
         codein = codein.toUpperCase();
         String apiUrl = "https://economia.awesomeapi.com.br/last/"+ code +"-"+ codein;
 
         //Configuração do HttpClient
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(apiUrl))
                 .GET()
                 .build();
 
         //Enviando a requisição e obtendo a resposta
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        dto = null;
+
         if (response.statusCode() == 200) {
-            //Processando o JSON com Jackson
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonResponse = response.body();
-
             //Extraindo o objeto USDBRL do JSON
-            dto = objectMapper.readTree(jsonResponse)
+            return objectMapper.readTree(response.body())
                     .get(code + codein)
                     .traverse(objectMapper)
                     .readValueAs(CurrencyDTO.class);
         }
-        return dto;
+       return null;
     }
 }
