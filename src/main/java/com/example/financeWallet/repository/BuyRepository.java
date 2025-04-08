39085039ac0223361710dcbe35db1ac363a1780f
adrlm <adriellima.barros@gmail.com>
2025-04-08 08:39:29 -0300

    query of calculation of average added

--- src/main/java/com/example/financeWallet/repository/BuyRepository.java
+++ src/main/java/com/example/financeWallet/repository/BuyRepository.java
@@ -9,4 +9,7 @@ import java.math.BigDecimal;
 public interface BuyRepository extends JpaRepository<BuyEntity, Long> {
     @Query("SELECT SUM(b.profit) FROM BuyEntity b")
     BigDecimal sumProfit();
+
+    @Query("SELECT AVG(b.cryptoValue) FROM BuyEntity b")
+    BigDecimal average();
 }
