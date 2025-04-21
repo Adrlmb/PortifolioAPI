97a0e9c9b1f120f06470e219a839b5b59d418246
adrlm <adriellima.barros@gmail.com>
2025-04-20 21:27:08 -0300

    unnecessary classes excluded

--- src/main/java/com/example/portfolioAPI/repository/CurrencyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.portfolioAPI.repository;
-
-import com.example.portfolioAPI.entity.CurrencyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyRepository extends JpaRepository<CurrencyEntity, Long> {
-}
