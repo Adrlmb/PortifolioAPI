3e656f974ba72481fb8aaca93c99ae663a3c468d
adrlm <@>
2025-06-17 03:43:50 -0300

    created Rest Controller with GetMapping and PostMapping methods

---
+++ src/main/java/com/example/portfolioAPI/users/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.portfolioAPI.users.controller;
+
+import com.example.portfolioAPI.users.dto.UserDTO;
+import com.example.portfolioAPI.users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<UserDTO> listAll(){
+        return service.listAll();
+    }
+
+    @PostMapping
+    public void insert(@RequestBody UserDTO dto){
+        service.insert(dto);
+    }
+
+}
