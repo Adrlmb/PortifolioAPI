338d240f3bd571a4a6226387814ecffbe1d8e293
adrlm <adriellima.barros@gmail.com>
2025-07-03 14:12:17 -0300

    fix: add missing import for ErrorResponse

--- src/main/java/com/example/portfolioAPI/exceptions/ErrorResponse.java
+++ src/main/java/com/example/portfolioAPI/exceptions/ErrorResponse.java
@@ -2,6 +2,8 @@ package com.example.portfolioAPI.exceptions;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class ErrorResponse{
