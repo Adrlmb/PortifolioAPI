a7d211abc53cbfa0ed303b77534efba391998484
adrlm <@>
2025-07-05 03:22:47 -0300

    refactor: return changed to ResponseEntity<ErrorResponse>, using ErrorResponse class to customize the message.

--- src/main/java/com/example/portfolioAPI/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/example/portfolioAPI/exceptions/CustomExceptionHandler.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(IdNotFoundException.class)
-    public ResponseEntity<String> handleIdNotFound(IdNotFoundException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleIdNotFound(IdNotFoundException ex){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
