52eb5b5a4732d34c3ebd804809561fcc7532ef40
adrlm <adriellima.barros@gmail.com>
2025-12-04 00:24:16 -0300

    adição de findByUserId

--- src/main/java/com/example/portfolioAPI/transactions/repository/BuyRepository.java
+++ src/main/java/com/example/portfolioAPI/transactions/repository/BuyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface BuyRepository extends JpaRepository<BuyEntity, Long> {
     @Query("SELECT SUM(b.profit) FROM BuyEntity b")
@@ -12,4 +13,6 @@ public interface BuyRepository extends JpaRepository<BuyEntity, Long> {
 
     @Query("SELECT AVG(b.cryptoValue) FROM BuyEntity b")
     BigDecimal average();
+
+    List<BuyEntity> findByUserId (Long userId);
 }
