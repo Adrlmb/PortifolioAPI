33bf588fe1ae7649d189de4144fc0745d5aa185b
adrlm <adriellima.barros@gmail.com>
2025-12-04 00:27:07 -0300

    criar DTO para token

---
+++ src/main/java/com/example/portfolioAPI/auth/dto/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.portfolioAPI.auth.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TokenResponse {
+    private String token;
+
+}
