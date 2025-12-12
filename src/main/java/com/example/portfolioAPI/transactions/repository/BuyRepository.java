235fd4e560fb425a9999f904fa4226474d1ab6dc
adrlm <adriellima.barros@gmail.com>
2025-12-11 22:17:27 -0300

    refactor(repository): adiciona métodos por usuário em BuyRepository
    - Remove findByUserId(Long)
    - Adiciona findAllByUser(UserEntity user)
    - Adiciona findByIdAndUser(Long id, UserEntity user)

--- src/main/java/com/example/portfolioAPI/transactions/repository/BuyRepository.java
+++ src/main/java/com/example/portfolioAPI/transactions/repository/BuyRepository.java
@@ -1,11 +1,14 @@
 package com.example.portfolioAPI.transactions.repository;
 
 import com.example.portfolioAPI.transactions.entity.BuyEntity;
+import com.example.portfolioAPI.users.entity.UserEntity;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface BuyRepository extends JpaRepository<BuyEntity, Long> {
     @Query("SELECT SUM(b.profit) FROM BuyEntity b")
@@ -14,5 +17,7 @@ public interface BuyRepository extends JpaRepository<BuyEntity, Long> {
     @Query("SELECT AVG(b.cryptoValue) FROM BuyEntity b")
     BigDecimal average();
 
-    List<BuyEntity> findByUserId (Long userId);
+    List<BuyEntity> findAllByUser (UserEntity user);
+
+    Optional<BuyEntity> findByIdAndUser(Long id, UserEntity user);
 }
